f7231c42ff55a2051bea820fcac972bbbff71eb0
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2020-08-23 10:24:02 +0200

    Working app

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # JavaScript Logic
 
-Use advanced logic through JavaScript
+## Give Homey the possibility to use JavaScript logic through flow cards
+
+- Check if specified text is equal to one of the values in an array (case sensitive)
+- Check if specified text contains one of the values in an array (case sensitive)
+- Check if specified text is empty
--- app.js
+++ app.js
@@ -5,7 +5,87 @@ const Homey = require('homey');
 class MyApp extends Homey.App {
 	
 	onInit() {
-		this.log('MyApp is running...');
+		this.log(Homey.manifest.name.en + " v" + Homey.manifest.version + " is running...");
+
+		new Homey.FlowCardCondition('value_in_array')
+            .register()
+            .registerRunListener((args, state) => {
+				if (args.array) {
+					let array = args.array.split(';');
+					//this.log('condition: Array items:', array);
+
+					if (args.value) {
+						let value;
+						if (typeof args.value == 'string') {
+							value = args.value.trim();
+						}
+						else {
+							value = args.value;
+						}
+
+						//this.log('condition: Value:', value);
+						let result = array.some(item => {
+							//this.log(`'${item}' == '${value}' :: ( (${typeof item}) == (${typeof value}) )`);
+							return (item == value);
+						});
+						//this.log('condition: Is value in array:', result);
+						return Promise.resolve(result);
+					}
+					else {
+						return Promise.resolve(false);
+					}
+				}
+				else {
+					return Promise.resolve(false);
+				}
+			});
+		
+		new Homey.FlowCardCondition('value_empty')
+			.register()
+			.registerRunListener((args, state) => {
+				if (args.value) {
+					//this.log(`condition: Value: '${args.value}'`);
+					let result = (args.value == undefined || args.value === '' || args.value === ' ')
+					//this.log('condition: Is value undefined or empty:', result);
+					return Promise.resolve(result);
+				}
+				else {
+					return Promise.resolve(true);
+				}
+			});
+		
+		new Homey.FlowCardCondition('value_contains_array')
+			.register()
+			.registerRunListener((args, state) => {
+				if (args.array) {
+					let array = args.array.split(';');
+					//this.log('condition: Array items:', array);
+
+					if (args.value) {
+						let value;
+						if (typeof args.value == 'string') {
+							value = args.value.trim();
+						}
+						else {
+							value = args.value;
+						}
+
+						//this.log('condition: Value:', value);
+						let result = array.some(item => {
+							//this.log(`'${item}'.includes('${value}') :: ( (${typeof item}) == (${typeof value}) )`);
+							return (item.includes(value));
+						});
+						//this.log('condition: Value contains one of:', result);
+						return Promise.resolve(result);
+					}
+					else {
+						return Promise.resolve(false);
+					}
+				}
+				else {
+					return Promise.resolve(false);
+				}
+			});
 	}
 	
 }
